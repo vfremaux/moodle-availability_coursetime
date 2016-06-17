6531d82e58571e3fd73fe5886ebd40ce0b731ef2
valery <valery.fremaux@gmail.com>
2016-06-17 11:33:49 +0200

    fix version

--- version.php
+++ version.php
@@ -18,14 +18,14 @@
  * Version info.
  *
  * @package availability_week
- * @copyright 2014 The Open University
+ * @copyright 2016 Valery Fremaux (valery.fremaux@gmail.com)
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016090600;
-$plugin->requires = 2014050800;
+$plugin->requires = 2015050500;
 $plugin->component = 'availability_coursetime';
-$plugin->release = '2.7.0 (Build 2016090600)';
+$plugin->release = '2.9.0 (Build 2016090600)';
 $plugin->dependencies = array('block_use_stats' => '*');
